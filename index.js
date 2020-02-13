d4daf1c32220ea2f38ad6d34c74f77323cd8b835
divyeshrana9775 <divyeshrana9775@gmail.com>
2020-02-13 18:59:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function reaction() { 
+    document.body.style.backgroundColor = 'red';
+    document.getElementById('btn1').style.backgroundColor='yellow';
+}
+const my = document.addEventListener('click', reaction);
+
